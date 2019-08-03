d19491eaa1d04a0dfb1c17118cc8d664a4b088dd
Brian <briandsalemi@gmail.com>
2019-08-03 09:44:56 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
-end
+    has_secure_password
+
+    validates :name, :password_digest, presence: true
+
+    has_many :rides 
+    has_many :attractions, through: :rides
+ 
+
+def mood
+    if self.happiness && self.nausea
+      if self.happiness >= self.nausea
+        return "happy"
+      else
+        return "sad"
+      end
+    end
+  end
+
+end 
